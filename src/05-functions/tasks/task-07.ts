8830d93334ed27d52cccbbc03263a0e9aa570ab4
Nabiel-Gallagher <deniza_fatih_34rpl@student.smktelkom-mlg.sch.id>
2026-08-20 08:45:51 +0700

    Mengerjakan Task 7

--- src/05-functions/tasks/task-07.ts
+++ src/05-functions/tasks/task-07.ts
@@ -39,18 +39,73 @@ const students = [
   }
 ];
 
-function countActiveStudents(...){
+function countActiveStudents(
+    students: { name: string; major: string; active: boolean }[]
+) {
+    let MahasiswaAktif: number = 0;
 
+    for (let i = 0; i < students.length; i++) {
+        let aktif = students[i];
+
+        if (aktif.active) {
+            MahasiswaAktif++;
+        }
+    }
+
+    return MahasiswaAktif;
 }
 
-function countInactiveStudents(...){
+function countInactiveStudents(
+    students: { name: string; major: string; active: boolean }[]
+) {
+    let MahasiswaNonAktif: number = 0;
 
+    for (let i = 0; i < students.length; i++) {
+        let nonAktif = students[i];
+
+        if (!nonAktif.active) {
+            MahasiswaNonAktif++;
+        }
+    }
+
+    return MahasiswaNonAktif;
 }
 
-function countStudentsByMajor(...){
+function countStudentsByMajor(
+    students: { name: string; major: string; active: boolean }[],
+    major: string
+) {
+    let count: number = 0;
 
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].major === major) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
-function printEnrollmentReport(...){
-    
+function printEnrollmentReport() {
+    console.log("Enrollment Report:");
+    console.log("Total students : " + students.length);
+    console.log("Active students : " + countActiveStudents(students));
+    console.log("Inactive students : " + countInactiveStudents(students));
+
+    console.log(
+        "Software Engineering students : " +
+        countStudentsByMajor(students, "Software Engineering")
+    );
+
+    console.log(
+        "Networking students : " +
+        countStudentsByMajor(students, "Networking")
+    );
+
+    console.log(
+        "Multimedia students : " +
+        countStudentsByMajor(students, "Multimedia")
+    );
 }
+
+printEnrollmentReport();
